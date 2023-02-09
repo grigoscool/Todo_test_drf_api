16bd07b085fb24a65a01941429472602b5e3e19a
Sergey Grigorev <grigoscool@mail.ru>
2023-02-09 11:58:13 +0300

    add in to_do app models Board and Todo

--- test/urls.py
+++ test/urls.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.urls import re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny, ],
 )
 
 urlpatterns = [
@@ -23,4 +23,6 @@ urlpatterns = [
     re_path(r'^swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('auth-drf', include('rest_framework.urls')),
+    path('', include('to_do.urls', namespace='to_do')),
 ]
--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    done = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Board(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    board = models.ForeignKey(Todo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
